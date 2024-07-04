af31ec381e6f428b03e7d7df42ea85d3beddc7e6
rr-bw <102181210+rr-bw@users.noreply.github.com>
2024-07-04 15:33:07 -0700

    add jsdocs

--- libs/auth/src/angular/set-password-jit/set-password-jit.service.abstraction.ts
+++ libs/auth/src/angular/set-password-jit/set-password-jit.service.abstraction.ts
@@ -3,6 +3,10 @@ import { UserId } from "@bitwarden/common/types/guid";
 import { PasswordInputResult } from "../input-password/password-input-result";
 
 export abstract class SetPasswordJitService {
+  /**
+   * Uses the output from InputPasswordComponent to set a password
+   * for a JIT provisioned user.
+   */
   setPassword: (
     passwordInputResult: PasswordInputResult,
     orgSsoIdentifier: string,
@@ -10,5 +14,12 @@ export abstract class SetPasswordJitService {
     resetPasswordAutoEnroll: boolean,
     userId: UserId,
   ) => Promise<void>;
+  /**
+   * Runs client-specific code after the password has been successfully set.
+   * This method is should be overridden in clients that need to run code
+   * after the password has been set.
+   * @see WebSetPasswordJitService
+   * @see DesktopSetPasswordJitService
+   */
   runClientSpecificLogicAfterSetPasswordSuccess: () => Promise<void> | null;
 }
