2df6e0294c7448911ba5f38421a960c4ed66a97e
Maciej Zieniuk <mzieniuk@bitwarden.com>
2024-07-04 15:24:20 +0200

    SM-1146: Tailwind states with groups

--- libs/components/src/tabs/tab-nav-bar/tab-link.component.ts
+++ libs/components/src/tabs/tab-nav-bar/tab-link.component.ts
@@ -1,13 +1,5 @@
 import { FocusableOption } from "@angular/cdk/a11y";
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  HostListener,
-  Input,
-  OnDestroy,
-  ViewChild,
-} from "@angular/core";
+import { AfterViewInit, Component, HostListener, Input, OnDestroy, ViewChild } from "@angular/core";
 import { IsActiveMatchOptions, RouterLinkActive } from "@angular/router";
 import { Subject, takeUntil } from "rxjs";
 
@@ -24,7 +16,6 @@ export class TabLinkComponent implements FocusableOption, AfterViewInit, OnDestr
 
   @ViewChild(TabListItemDirective) tabItem: TabListItemDirective;
   @ViewChild("rla") routerLinkActive: RouterLinkActive;
-  @ViewChild("endSlot") endSlot: ElementRef<HTMLElement>;
 
   readonly routerLinkMatchOptions: IsActiveMatchOptions = {
     queryParams: "ignored",
@@ -44,10 +35,6 @@ export class TabLinkComponent implements FocusableOption, AfterViewInit, OnDestr
 
   get active() {
     return this.routerLinkActive?.isActive ?? false;
-  }
-
-  get haveEndSlot() {
-    return this.endSlot?.nativeElement?.childElementCount !== 0;
   }
 
   constructor(private _tabNavBar: TabNavBarComponent) {}
