51b85b31b95e04d13416e22f6c290707a8cc8aa9
Vicki League <vleague@bitwarden.com>
2024-07-09 16:20:09 -0400

    [CL-285] Add default loading state to popup-page

--- apps/browser/src/platform/popup/layout/popup-page.component.ts
+++ apps/browser/src/platform/popup/layout/popup-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, Input, OnInit } from "@angular/core";
+
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 
 @Component({
   selector: "popup-page",
@@ -7,5 +10,15 @@ import { Component } from "@angular/core";
   host: {
     class: "tw-h-full tw-flex tw-flex-col tw-flex-1 tw-overflow-y-auto",
   },
+  imports: [CommonModule],
 })
-export class PopupPageComponent {}
+export class PopupPageComponent implements OnInit {
+  constructor(protected i18nService: I18nService) {}
+
+  @Input() loading = false;
+  @Input() loadingText?: string;
+
+  ngOnInit() {
+    this.loadingText = this.loadingText ?? this.i18nService.t("loading");
+  }
+}
